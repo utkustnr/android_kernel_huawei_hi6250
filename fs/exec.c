e4b64c47380b059793a757f699e8e8b2fec56146
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-22 22:37:25 +0800

    KernelSU: integrate scope-minimized manual hooks
    This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks.
    This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat).
    It reduces the scope of kernel function interception and still maintains full fucntionality.
    
    This commit is a squash of the following:
    *	fs/exec: do_execve: ksu_handle_execveat hook
    *	fs/exec: compat_do_execve: ksu_handle_execveat_sucompat hook
    	fs/open: sys_faccessat: ksu_handle_faccessat hook
    *	fs/read_write: sys_read: ksu_handle_sys_read hook
    *	fs/stat: sys_newfstatat: ksu_handle_stat hook
    *	fs/stat: sys_fstatat64: ksu_handle_stat hook
    *	drivers: input: input_event: ksu_handle_input_handle_event hook
    *	drivers: tty/pty.c: pts_unix98_lookup: ksu_handle_devpts hook
    
    references: KernelSU pr#1657, pr#2084
    	https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1874,6 +1874,15 @@ out_ret:
 	return retval;
 }
 
+
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+							   void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+										void *argv, void *envp, int *flags);
+#endif
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
@@ -1893,6 +1902,12 @@ int do_execve(struct filename *filename,
 	}
 	return execve_ret;
 #else
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	else
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 #endif
 }
@@ -1921,6 +1936,11 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+
+#ifdef CONFIG_KSU
+	if (!ksu_execveat_hook)
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL); /* 32-bit support */
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
